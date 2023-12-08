359d289b42f95dee39c771314bc07fa4fee68248
Schuetze <Benjamin.Schuetze@scheer-group.com>
2023-12-08 12:28:06 +0100

    wipü ideas page

--- src/components/AddIdeaModal.tsx
+++ src/components/AddIdeaModal.tsx
@@ -1,13 +1,85 @@
-import { IonModal } from "@ionic/react";
+import {
+  IonModal,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonContent,
+  IonInput,
+  IonTextarea,
+} from "@ionic/react";
+import { useHistory } from "react-router-dom";
+import { supabase } from "../../supabase";
+
+const inputStyle = {
+  "--padding-start": "10px",
+  "--padding-end": "10px",
+  "--border-radius": "8px",
+  "--border": "2px solid white",
+  "--background": "#f0f0f0",
+
+  "--color": "black",
+  marginBottom: "16px",
+};
+
+const textareaStyle = {
+  ...inputStyle,
+  "--height": "100vh",
+  "--resize": "none",
+};
+
 import { useRef } from "react";
-const AddIdeaModal = () => {
+const AddIdeaModal = ({ groupID }) => {
   const modal = useRef<HTMLIonModalElement>(null);
+  const descriptionInput = useRef<HTMLIonTextareaElement>(null);
+
+  const history = useHistory();
+
+  const addIdea = async () => {
+    const ideaText = descriptionInput.current?.value;
+
+    if (ideaText) {
+      const { data, error } = await supabase
+        .from("ideas")
+        .insert({ text: ideaText, group_id: groupID })
+        .select();
+      console.log("data: ", { data, error });
+      if (!error) {
+        modal.current?.dismiss();
+        history.go(0);
+      }
+    }
+  };
 
   return (
     <IonModal trigger="open-add-idea-modal" ref={modal}>
-      <div>
-        <span>HALLO IDEA MODAL</span>
-      </div>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonButton onClick={() => modal.current?.dismiss()}>
+              Cancel
+            </IonButton>
+          </IonButtons>
+
+          <IonButtons slot="end">
+            <IonButton strong={true} onClick={() => addIdea()}>
+              Add Idea
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <div>
+          <IonTextarea
+            style={textareaStyle}
+            ref={descriptionInput}
+            placeholder="Describe your idea shortly..."
+            autoGrow={true}
+            counter={true}
+            maxlength={500}
+          ></IonTextarea>
+        </div>
+      </IonContent>
     </IonModal>
   );
 };
--- src/pages/GroupPage.tsx
+++ src/pages/GroupPage.tsx
@@ -4,42 +4,66 @@ import { supabase } from "../../supabase";
 import AddButton from "../components/AddButton";
 import AddIdeaModal from "../components/AddIdeaModal";
 
+interface RouteParams {
+  id: string;
+}
+
 const GroupPage = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [ideas, setIdeas] = useState([]);
 
-  const { id } = useParams();
+  const { id }: RouteParams = useParams();
 
   const getGroupData = async () => {
-    const { data, error } = await supabase.from("groups").select().eq("id", id);
+    try {
+      const { data } = await supabase.from("groups").select().eq("id", id);
 
-    if (data?.length === 0) {
+      if (data?.length === 0) {
+        setLoading(false);
+        setError(true);
+        return;
+      }
       setLoading(false);
-      setError(true);
-      return;
+    } catch (e) {
+      console.log("ERROR: Error while getting Group Data: ", e);
     }
-
-    setLoading(false);
   };
 
-  console.log("Group Id: ", id);
+  const getIdeasForGroup = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("ideas")
+        .select()
+        .eq("group_id", id);
+
+      console.log("data: ", { data, error });
+    } catch (e) {
+      console.log("ERROR: Request Error: ", e);
+    }
+  };
 
   useEffect(() => {
-    getGroupData();
-  }, []);
+    // only supposed to get data on first render
+    if (loading) getGroupData();
+
+    // if user is authorized to fetch he can load the ideas
+    if (!error) getIdeasForGroup();
+  }, [error]);
 
   if (loading) {
     return <div>Loading...</div>;
   }
+
   if (error) {
     return <div>Something went wrong</div>;
   }
 
   return (
     <div>
-      <span>Hallo Gruppenmitglied</span>
+      <span>Hallo Gruppenmitglied </span>
       <AddButton openID={"open-add-idea-modal"} />
-      <AddIdeaModal />
+      <AddIdeaModal groupID={id} />
     </div>
   );
 };
